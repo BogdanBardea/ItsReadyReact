983230b5e394984e9ddd20944e6e42308db7262a
Bogdan Bârdea <bogdanbardea@gmail.com>
2020-07-08 17:53:06 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
-} from "react-router-dom";
-import AsideMenu from "./components/AsideMenu/AsideMenu";
-import Header from "./components/Header/Header";
-import HomePage from "./pages/HomePage/HomePage";
-import Settings from "./pages/Settings/Settings";
-import Merchants from "./pages/Merchants/Merchants";
-import Users from "./pages/Users/Users";
-import Notifications from "./pages/Notifications/Notifications";
-import { createBrowserHistory } from "history";
-import LoginPage from "./pages/Login/Login";
-import ResetPassword from "./pages/ForgotPassword/ForgotPassword";
-import EmailSent from "./pages/ForgotPasswordEmailSent/ForgotPasswordEmailSent";
-import SettingsBtw from "./pages/SettingsBtw/SettingsBtw"
+} from 'react-router-dom';
+import AsideMenu from './components/AsideMenu/AsideMenu';
+import Header from './components/Header/Header';
+import HomePage from './pages/HomePage/HomePage';
+import Settings from './pages/Settings/Settings';
+import Merchants from './pages/Merchants/Merchants';
+import Users from './pages/Users/Users';
+import Notifications from './pages/Notifications/Notifications';
+import { createBrowserHistory } from 'history';
+import LoginPage from './pages/Login/Login';
+import ResetPassword from './pages/ForgotPassword/ForgotPassword';
+import EmailSent from './pages/ForgotPasswordEmailSent/ForgotPasswordEmailSent';
+import SettingsBtw from './pages/SettingsBtw/SettingsBtw';
 const App = (props) => {
   const history = createBrowserHistory();
   const [token, setToken] = useState(
-    localStorage.getItem("token") ? localStorage.getItem("token") : ""
+    localStorage.getItem('token') ? localStorage.getItem('token') : ''
   );
   const [user, setUser] = useState(
-    localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {}
+    localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : {}
   );
   return (
     <Router>
-      {history.location.pathname == "/login" ||
-      history.location.pathname == "/reset_password" ||
-      history.location.pathname == "/email_sent" ? (
+      {history.location.pathname == '/login' ||
+      history.location.pathname == '/reset_password' ||
+      history.location.pathname == '/email_sent' ? (
         <Switch>
           <Route path="/login">
             <LoginPage
@@ -69,19 +69,19 @@ const App = (props) => {
         <div>
           <div
             className="row"
-            style={{ maxWidth: "100%", margin: "0px", padding: "0px" }}
+            style={{ maxWidth: '100%', margin: '0px', padding: '0px' }}
           >
             <div className="col-2">
               <AsideMenu />
             </div>
-            <div className="col-10" style={{ paddingRight: "0px" }}>
+            <div className="col-10" style={{ paddingRight: '0px' }}>
               <Header
                 user={user}
                 setUser={setUser}
                 token={token}
                 setToken={setToken}
               />
-              <div className="row" style={{ maxWidth: "100%" }}>
+              <div className="row" style={{ maxWidth: '100%' }}>
                 <div className="col-12">
                   <Switch>
                     <Route path="/login">
@@ -112,10 +112,10 @@ const App = (props) => {
                       <HomePage token={token} setToken={setToken} />
                     </Route>
                   </Switch>
-                </div>{" "}
-              </div>{" "}
-            </div>{" "}
-          </div>{" "}
+                </div>{' '}
+              </div>{' '}
+            </div>{' '}
+          </div>{' '}
         </div>
       )}
     </Router>
--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './DatePicker.css';
+import DateRangePicker from '@wojtekmaj/react-daterange-picker';
+import calendar from '../../assets/calendar.png';
 
-const DatePicker = () => {
+function DatePicker() {
+  const [value, onChange] = useState([new Date(), new Date()]);
+
   return (
-    <div>
-      <h3>Date Picker Component</h3>
+    <div className="data-range-picker">
+      <DateRangePicker
+        onChange={onChange}
+        value={value}
+        clearIcon={null}
+        calendarClassName="data-range-picker"
+        calendarIcon={null}
+      />
+      <img src={calendar} alt="calendar icon" className="calendar-icon" />
     </div>
   );
-};
+}
 
 export default DatePicker;
